39bba4cdcaa399c53c5c28387a56e887e1df25c8
srsuper <aot5310@gmail.com>
2021-04-12 15:21:36 +0700

    Create liff.js

---
+++ liff.js
@@ -0,0 +1,15 @@
+        liff.ready
+      .then(() => {
+        if (liff.isLoggedIn()) {
+          return liff.getProfile();
+        } else {
+          liff.login({
+            redirectUri: "https://script.google.com/macros/s/AKfycbxmScI2wPGEGxg1fad42fDs4Q-3EpxxSxvLkTv8tGoXNNa3B0sdk4BAc9on83EC4GCF/exec",
+          });
+        }
+      })
+      .then((profile) => {
+        window.location.replace(
+          `https://docs.google.com/forms/d/e/1FAIpQLSfqXxAzWKInuEHOBx9wBqAJ97RrAYQGU6jU0gm7uiIdOfekbQ/viewform?usp=pp_url&entry.412902974=${profile.displayName}`
+        );
+      });
